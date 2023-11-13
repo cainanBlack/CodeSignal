94347dc31a94ad90f8ae97d8848f5c3530061576
Cainan Black <92112505+cainanBlack@users.noreply.github.com>
2023-11-12 22:33:35 -0500

    Upload of additional challenges and LeetCode

---
+++ Sudoku/Sudoku.py
@@ -0,0 +1,90 @@
+"""
+Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with digits so that each column, each row, and each of the nine 3 × 3 sub-grids that compose the grid contains all of the digits from 1 to 9.
+
+This algorithm should check if the given grid of numbers represents a correct solution to Sudoku.
+
+Example
+
+For
+grid = [[1, 3, 2, 5, 4, 6, 9, 8, 7],
+        [4, 6, 5, 8, 7, 9, 3, 2, 1],
+        [7, 9, 8, 2, 1, 3, 6, 5, 4],
+        [9, 2, 1, 4, 3, 5, 8, 7, 6],
+        [3, 5, 4, 7, 6, 8, 2, 1, 9],
+        [6, 8, 7, 1, 9, 2, 5, 4, 3],
+        [5, 7, 6, 9, 8, 1, 4, 3, 2],
+        [2, 4, 3, 6, 5, 7, 1, 9, 8],
+        [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+the output should be
+solution(grid) = true;
+
+For
+grid = [[8, 3, 6, 5, 3, 6, 7, 2, 9],
+        [4, 2, 5, 8, 7, 9, 3, 8, 1],
+        [7, 9, 1, 2, 1, 4, 6, 5, 4],
+        [9, 2, 1, 4, 3, 5, 8, 7, 6],
+        [3, 5, 4, 7, 6, 8, 2, 1, 9],
+        [6, 8, 7, 1, 9, 2, 5, 4, 3],
+        [5, 7, 6, 9, 8, 1, 4, 3, 2],
+        [2, 4, 3, 6, 5, 7, 1, 9, 8],
+        [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+the output should be
+solution(grid) = false.
+
+The output should be false: each of the nine 3 × 3 sub-grids should contain all of the digits from 1 to 9.
+These examples are represented on the image below.
+
+
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[memory limit] 1 GB
+
+[input] array.array.integer grid
+
+A matrix representing 9 × 9 grid already filled with numbers from 1 to 9.
+
+Guaranteed constraints:
+grid.length = 9,
+grid[i].length = 9,
+1 ≤ grid[i][j] ≤ 9.
+
+[output] boolean
+
+true if the given grid represents a correct solution to Sudoku, false otherwise.
+
+
+Solution: Cainan Black
+"""
+
+def solution(grid):
+    rowset = set()
+    colset = set()
+    for i in range(9):
+        rowset = set()
+        colset = set()
+        for j in range(9):
+            rowset.add(grid[i][j])
+            colset.add(grid[j][i])
+        if len(rowset) != 9:
+            return False
+        if len(colset) != 9:
+            return False
+    import itertools
+    for i, j in itertools.product(range(0,8,3), range(0,8,3)):
+        
+        _set = add_elements_to_set(grid, i,j)
+        if len(_set) != 9:
+            return False
+    
+    
+    return True
+
+def add_elements_to_set(grid, m, n):
+    _set = set()
+    for i in range(m, m+3):
+        for j in range(n, n+3):
+            _set.add(grid[i][j])
+    return _set
